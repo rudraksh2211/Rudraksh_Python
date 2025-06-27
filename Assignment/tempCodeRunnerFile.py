36e3e1df2b33d22e2687a4dc286869be388a2423
Rudraksh Dusad <rudraksh.dusad2211@gmail.com>
2025-06-27 20:49:44 +0530

    Assignment 8

--- Assignment/tempCodeRunnerFile.py
+++ Assignment/tempCodeRunnerFile.py
@@ -1,6 +1,5 @@
-import sqlite3
 
-conn=sqlite3.connect("ass4.db")
-conn.execute(''' 
-             update teeacher where name="Rudraksh Dusad" set salary="300000
-             ''')
+print(arr1)
+
+arr=np.empty(10)
+print(arr)
--- Practice&Notes/numpy_code.py
+++ Practice&Notes/numpy_code.py
@@ -52,3 +52,11 @@ print(a[3]+a[4])
 a=np.array([[1,2,3,4,5],[6,7,8,9,10]])
 print(a[0,3])
 print(a[-1,-3])
+
+#slicing
+a=np.array([1,2,3,4,5])
+print(a[1:4])
+print(a[::2])
+
+a=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(a[0:2,1:4:2])
